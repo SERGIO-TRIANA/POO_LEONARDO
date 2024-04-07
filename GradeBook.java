a9da92710616dcb0a5655b357704a8fb0e3565fc
Sergio Andres Rodriguez Triana <144968844+SERGIO-TRIANA@users.noreply.github.com>
2024-04-07 14:07:24 -0500

    This two is with sentinel method
    as you can see this two files of java was done with sentinel method to calculate the grades numbers, grades average, and total grades.

--- GradeBook.java
+++ GradeBook.java
@@ -44,6 +44,37 @@ public class GradeBook {
         System.out.println("\n Total of all 10 grades is: " + total);
         System.out.println("Class average is: " + average);
     }
+    public void determineClassAverage2()/*sentinel methods */ {
+        Scanner input = new Scanner(System.in);
+
+        int total;
+        int gradeCounter;
+        int grade;
+        double average;
+
+        total = 0;
+        gradeCounter = 0;
+
+        System.out.print("Enter grade or -1 to quit: ");
+        grade = input.nextInt();
+
+        while (grade != -1) {
+
+            total = total + grade;
+            gradeCounter = gradeCounter + 1;
+
+            System.out.print("Enter grade or -1 to quit: ");
+            grade = input.nextInt();
+        }
+        if (gradeCounter != 0)
+        {
+            average = (double) total / gradeCounter;
+            System.out.println("Total of the " + gradeCounter + " grades entered is: " + total);
+            System.out.println("Class average is " + average);
+        }
+        else System.out.println("No grades were entered");
+    }
+
 }
 /*
     public GradeBook() {
