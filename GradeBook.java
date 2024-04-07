303c4e7ae0190a2795939bd70cf2b27b1b4020d5
Sergio Andres Rodriguez Triana <144968844+SERGIO-TRIANA@users.noreply.github.com>
2024-04-07 12:47:07 -0500

    Version Mejorada
    Aca puede ver una version mejorada del ejercicio 1

--- GradeBook.java
+++ GradeBook.java
@@ -4,11 +4,39 @@ public class GradeBook {
 
     private String courseName;
 
+    public GradeBook(String name) {
+        courseName = name;
+    }
+
+    public void setCourseName(String name) {
+        courseName = name;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void displayMessage() {
+        System.out.println("Welcome to the grade book for: " + getCourseName());
+    }
+}
+/*
     public GradeBook() {
     }
 
+    public static long promEstudiante(long a, long b, long c, long d, long e) {
+        long prom = (a + b + c + d + e) / 5;
+
+        return prom;
+    }
+
+    ;
+
+
     public GradeBook(String cs102DataStructuresInJava) {
     }
+}
+
 
     public String getCourseName() {
         return courseName;
@@ -18,9 +46,7 @@ public class GradeBook {
         this.courseName = courseName;
     }
 
-    public String displayMessage()
-    {
-        return  "Welcome to the grade book " + getCourseName();
 
-    }
 }
+
+ */
